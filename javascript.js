e649b5f861fa3d367c88d464f5a865b14e1fcff7
chera-mehdi <cheramehdi7@gmail.com>
2022-08-17 17:59:32 +0100

    finished

---
+++ javascript.js
@@ -0,0 +1,102 @@
+let screen = document.querySelector(".screen");
+let result = document.createElement("p");
+let num = "";
+let num2 = "";
+let operator = "";
+function add (num,num2){
+  let result = num + num2;
+  return result;
+}
+function subtract (num,num2){
+  let result = num - num2;
+  return result;
+}
+function multiply (num,num2){
+  let result = num * num2;
+  return result;
+}
+function divide (num,num2){
+  if(num2 === 0){
+    return "error";
+  }
+  else{
+  let result = num / num2;
+  return result.toFixed(2);
+}
+}
+function operate (num,operator,num2){
+  if (operator == "+"){
+  return add (num,num2);
+  }
+  else if (operator == "-") {
+  return subtract (num,num2);
+  }
+  else if (operator == "*") {
+  return multiply (num,num2);
+  }
+  else {
+  return divide (num,num2);
+};
+}
+function equal(){
+  num2 = (+screen.textContent);
+  if(num === ""|| num2 === ""|| operator === ""){
+    return false;
+  }
+  else{
+  screen.textContent="";
+  let value = operate(num,operator,num2);
+  result.textContent =value;
+  screen.appendChild(result);
+  num = "";
+  num2 = 0;
+}
+}
+function clear(){
+  num2 = "";
+  num = "";
+  operator = "";
+  screen.textContent="";
+  result.textContent ="";
+}
+function display(e){
+  result.textContent="";
+  screen.textContent+=e.target.innerText;
+}
+function operatorsfuns(e){
+  if(num === ""){
+    num = (+screen.textContent);
+    operator =e.target.innerText;
+    screen.textContent = "";
+  }
+  else if(num === "" && screen.textContent === ""){
+     num = (+result.textContent);
+     operator = e.target.innerText;
+     result.textContent = "";
+  }
+  else{
+    num2 = (+screen.textContent);
+    screen.textContent=""
+    let value = operate(num,operator,num2);
+    result.textContent =value;
+    screen.appendChild(result);
+    num = value;
+    num2 = "";
+    operator = e.target.innerText;
+  }
+}
+function operations(){
+  let buttons = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+  let equalbtn = document.querySelector(".equal");
+  let clearbtn = document.querySelector(".clear");
+  buttons.forEach(function(item){
+    item.addEventListener('click',display)
+  });
+  operators.forEach(function (item){
+    item.addEventListener('click',operatorsfuns)
+  });
+  equalbtn.addEventListener('click',equal);
+  clearbtn.addEventListener('click',clear);
+}
+operations();
